4fbcddea22a8429c5dd871c3b1d41ceb32acb887
Sarah Besozzi <sarah.besozzi@gmail.com>
2015-10-02 14:43:23 -0400

    Worked on Question 5 & 6

--- js/main.js
+++ js/main.js
@@ -1,15 +1,11 @@
-// ********* Question #1 ************
+
 
 // Create wrapped function
 
 (function () {
 
-  // console.log(items.length);
-
-    // items.forEach( function (item) {
-    //   console.log (item.price);
-    // });
-
+// **********************************
+// ********* Question #1 ************ 
 
 // Create an array of just the prices
 
@@ -55,17 +51,10 @@ var textNode = document.createTextNode(str);
 answer1.appendChild(textNode);
 
 
-}());
-
-
-
-
-
+// *********************************
 // ********* Question #2 ************
 
-(function () {
-
-  
+// (function () {
 
 // Create an array of the prices
 
@@ -79,80 +68,148 @@ var prices = items.map( function (item) {
 
 //Filter prices between 14 and 18
 
-var prices = items.filter (function (item) {
+var itemscost = items.filter (function (item) {
   return (item.price > 14) && (item.price < 18);
 
 });
 
- console.log(prices);
+ console.log(itemscost);
 
 
  // For each item make a string
+ var answer2 = document.querySelector('#answer2');
 
-var str = items.forEach (function (item) {
-  return (item.title);
-  
-});
+itemscost.forEach (function (item) {
 
-   console.log(str);
-
-
-// // Make it show up on the page
-
-var answer2 = document.querySelector('#answer2');
-var textNode = document.createTextNode(str);
+  console.log(item.title);
+  // return item.title;
+  var textNode = document.createTextNode(item.title);
 
 answer2.appendChild(textNode);
-
-
-}());
-
-
-// ************ Question 3 ********
-
-
-(function () {
-
-// Create an array of the currency
-
-var currency = items.map( function (item) {
-  return item.currency_code;
-  
 });
 
- console.log(currency);
+
+
+// **********************************
+// ************ Question 3 **********
+
+
+
 
  //Find GBP in array
 
- var currency = items.filter (function (item) {
+ var britishitems = items.filter (function (item) {
   return item.currency_code === "GBP";
 
 });
 
- console.log(currency);
+ console.log(britishitems);
 
 
-// For each item make a string
-
-// var str = item.title + ' costs ' + '£ ' + item.price;
-
-var str = items.list (function (item) {
-  return item.title + ' costs ' + '£ ' + item.price;
-
-});
-
-   console.log(str);
-
-
-
-
-// // Make it show up on the page
+// Make it show up on the page
 
 var answer3 = document.querySelector('#answer3');
-var textNode = document.createTextNode(str);
+
+
+britishitems.forEach (function (item) {
+  console.log(item.title);
+  
+var textNode = document.createTextNode(item.title);
 
 answer3.appendChild(textNode);
 
+});
+
+
+
+// *****************************
+// ******* Question 4 **********
+
+
+ //Find array items made of wood
+
+ var woodItems = items.filter (function(item) {
+    return item.materials.indexOf('wood' || 'wooden') !== -1;
+
+});
+
+ console.log(woodItems);
+
+
+// Make it show up on the page
+
+var answer4= document.querySelector('#answer4');
+
+
+woodItems.forEach (function (item) {
+  console.log(item.title);
+  
+var textNode = document.createTextNode(item.title);
+
+answer4.appendChild(textNode);
+
+});
+
+// *********************************
+// ********** Question 5 ***********
+
+// Array of items made of 8 or more materials
+
+var  materialItems = items.filter (function(item) {
+    return item.materials.length >=8;
+});
+
+console.log(materialItems);
+
+
+// Show item.name, # of item.materials length and item.materials
+
+var answer5= document.querySelector('#answer5');
+
+
+materialItems.forEach (function (item) {
+
+  console.log(item.title + item.materials.indexOf('Array') + item.materials);
+
+
+var textNode = document.createTextNode(item.title);
+
+answer5.appendChild(textNode);
+
+});
+
+
+// *************************************
+// ************** Question 6 ***********
+
+//
+// Find  seller made in array
+
+var sellerMade = items.filter (function (item) {
+  return item.who_made === "i_did";
+
+});
+
+  console.log(sellerMade.length);
+
+
+// Convert array to string
+
+var str = sellerMade.length + ' were made by their sellers';
+
+
+// Make it show up on the page
+
+
+var answer6 = document.querySelector('#answer6');
+
+  
+var textNode = document.createTextNode(str);
+
+answer6.appendChild(textNode);
+
+});
 
 }());
+
 
